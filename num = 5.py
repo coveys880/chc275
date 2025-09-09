730ad52ee1ee2f420ec6f740444924de191abd0f
coveys880 <coveys27@chcstudent.com>
2025-09-09 09:29:10 -0400

    Update num = 5.py

--- num = 5.py
+++ num = 5.py
@@ -8,3 +8,28 @@ print (x - num)
 print(num * x)
 print(x / num)
 print (sum)
+x = 5
+y = 10
+z = 0
+print (z)
+name = "Shane Covey"
+occupation = "go getter"
+print (name)
+print (occupation)
+print(f"My name is {name}, and my occcupation is (occupation)")
+first = "Calvert" \
+
+second = "job"
+school = first + second 
+print (school)
+print ("welcome to calvert hall")
+name = input ("what is your name?")
+gradyear = input ("what year do you graduate")
+standing = input ("what is your email")
+
+
+
+x = input("what is num1:")
+y = input("what is num2:") 
+z = x + y
+print(z)
