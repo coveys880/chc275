a94ab5cf500cf2b3508b8578eba23acd338a444c
coveys880 <coveys27@chcstudent.com>
2025-09-11 09:29:25 -0400

    commit

--- .py
+++ .py
@@ -3,22 +3,22 @@ y = input ("num2:")
 x = int(x)
 y = int(y)
 sum = x + y
-print (f "the sum of {x} + {y} + {sum}") 
+print (f"the sum is {sum}") 
 e = input ("num3")
 u = input ("num4") 
 e = int(e) 
 u = int(u)
 difference = e - u
-print (f "the difference of {e} - {u} - {difference}")
+print (f"the difference is {difference}")
 x = input ("num1:")
 y = input ("num2:")
 x = int(x)
 y = int(y)
-sum = x + y
-print (f "the product of {x} * {y} * {sum}") 
+product = x * y
+print (f"the product is {product}") 
 e = input ("num3")
 u = input ("num4") 
 e = int(e) 
 u = int(u)
-product = e / u
-product (f "the difference of {e} - {u} - {difference}")
+quotiant = e / u
+print(f"the quotiant is {quotiant}")
---
+++ branching.py
@@ -0,0 +1,16 @@
+x = 10
+if x % 2 == 0:
+    print(f"{x} is divisable by 2")
+    print ("test)")
+else: 
+    print ("this is not a part of the if statment")
+    
+x = 10 
+if x % 5 == 0: 
+        print(f"{x} is divisable by 5")
+elif x % 2 == 0: 
+        print(f"{x} is divisable by 2")
+else: 
+        print(f"{x} is not divisable by by 5 or 2")
+              
+          
