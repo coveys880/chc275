0ce021d877d079a820f13049d91d88f40b2142fc
coveys880 <coveys27@chcstudent.com>
2025-09-11 08:55:36 -0400

    Create .py

---
+++ .py
@@ -0,0 +1,24 @@
+x = input ("num1:")
+y = input ("num2:")
+x = int(x)
+y = int(y)
+sum = x + y
+print (f "the sum of {x} + {y} + {sum}") 
+e = input ("num3")
+u = input ("num4") 
+e = int(e) 
+u = int(u)
+difference = e - u
+print (f "the difference of {e} - {u} - {difference}")
+x = input ("num1:")
+y = input ("num2:")
+x = int(x)
+y = int(y)
+sum = x + y
+print (f "the product of {x} * {y} * {sum}") 
+e = input ("num3")
+u = input ("num4") 
+e = int(e) 
+u = int(u)
+product = e / u
+product (f "the difference of {e} - {u} - {difference}")
