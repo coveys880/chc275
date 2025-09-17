df435cdeac0d7a8ef9cb6be44ab824c9b4224543
coveys880 <coveys27@chcstudent.com>
2025-09-17 09:31:08 -0400

    commit

--- calculator.py
+++ calculator.py
@@ -1,6 +1,8 @@
+    
+
+calculator = input ("welcome to the calculator!")
+
 print("1. Addition")
 print ("2. Subtraction")
 print("3. Multipication")
 print ("4. Division")    
-
-calculator = input ("welcome to the calculator")
