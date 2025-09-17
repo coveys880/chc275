902d2b88c9ecdaefe13b364a03533d2483262477
coveys880 <coveys27@chcstudent.com>
2025-09-17 08:54:59 -0400

    com,mmit

--- branching.py
+++ branching.py
@@ -13,4 +13,25 @@ elif x % 2 == 0:
 else: 
         print(f"{x} is not divisable by by 5 or 2")
               
-          
+if x > 5 and x > 15:
+        print(f"{x} is greater thjan 5 and 15")
+        
+if x > 5 and x > 15:
+        print(f"{x} is greater thjan 5 and 15")
+        
+year = input("Enter your year") 
+
+if year == "freshman":
+        print("your grade level is 9") 
+elif year == "sophmore":
+        print("your grade level is 10")
+elif year == "junior":
+        print("your grade level is 11")
+elif year == "senior":
+        print("your grade level is 12")
+    
+print("welcome to the calculator")
+print("1. Addition")
+print ("2. Subtraction")
+print("3. Multipication")
+print ("4. Division")    
---
+++ calculator.py
@@ -0,0 +1,6 @@
+print("1. Addition")
+print ("2. Subtraction")
+print("3. Multipication")
+print ("4. Division")    
+
+calculator = input ("welcome to the calculator")
