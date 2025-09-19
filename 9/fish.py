145a5ba23774fb33ce870f8f412a4975f7c0d8b3
coveys880 <coveys27@chcstudent.com>
2025-09-19 13:31:15 -0400

    Update fish.py

---
+++ 9/fish.py
@@ -0,0 +1,14 @@
+x = input ("what kind of fish do you have?")
+if x == "carnivorous": 
+    y = input ("do you have it?") 
+    if y == "no":
+        print ("enjoy")
+    if y == "yes":
+        print ("to bad")
+elif  x == "salt water" :
+    print ("your a fancy fish parent")
+elif x == "community": 
+    print ("you should get more than one")
+else: 
+    print ("Idon't think thats a type of fish")
+    
