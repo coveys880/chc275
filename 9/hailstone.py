435bcef57dfe16a1715446795199cf621e6578ef
coveys880 <coveys27@chcstudent.com>
2025-09-22 09:52:39 -0400

    .py

--- 9/hailstone.py
+++ 9/hailstone.py
@@ -1,2 +1,12 @@
 x = input("what is x")
-x = int 
+x = int (x)
+check = False 
+while check == False:
+    if x % 2 == 0:
+        x = x/2
+        print(x)
+    else: 
+        x = x * 3 + 1
+        print (x)
+    if x == 1:
+        check = True
---
+++ 9/powerlevel.py
@@ -0,0 +1,11 @@
+x = input ("what is your first number")
+y = input ("what is your second number")
+x = int(x)
+y = int(y)
+if x > y:
+    print("number 1 win!")
+if x < y: 
+    print("number 2 wins!")
+if x == y: 
+    print("its a tie!")
+    
