1c9383c33bf9160d5695ebea610bef97a75b5687
coveys880 <coveys27@chcstudent.com>
2025-09-22 10:14:17 -0400

    .py

---
+++ 9/shane.py
@@ -0,0 +1,6 @@
+x = input("what are you searching for?")
+if x == ("Im seeking Knowledge"):
+    y =input("are you sure?")
+    if y == ("yes"):
+        print("you have been warned")
+else: print("you cant come any further")
