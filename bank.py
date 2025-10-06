2229f41eef51aefa1c5ade8fa29f68dfae7beb43
coveys880 <coveys27@chcstudent.com>
2025-10-06 09:31:20 -0400

    bank.py

--- bank.py
+++ bank.py
@@ -1,5 +1,5 @@
-names = []
-balences = []
+names = ["Shane"]
+balences = ["10"]
 check = False 
 while check == False: 
     x = input("do you want to see the menu?")
@@ -13,23 +13,38 @@ while check == False:
 
         action = input ("what do you want to do today?")
         if action == ("add account"):
-            print("what is your name?")
-        
-    elif action == "deposit":
-        acctname = input("what account will you like to deposit into?")
-    
-    elif action == "withdraw": 
-        acctname = input("what account will you want to withdraw from?")
-        
-    elif action == "transfer": 
-        acctname= input("what account do you want to transfer too?")
-        
-    elif action == "remove account": 
-        acctname = input("what account do you wish to remove?")    
-        
-         
-        
-    elif action == "quit": 
-            Check = True 
+           acctname = input("what is your account name?")
+           names.append(acctname)
+           balences.append(0)
+        elif action == "deposit":
+            acctname = input("what account will you like to deposit into?")
+            index = names.index(acctname)
+            money = input ("how much would you want to deposit")
+            money = int(money)
+            balences [index] = balences [index] + money 
+        elif action == "withdraw": 
+            acctname = input("what account will you want to withdraw from?")
+            index = names.index(acctname)
+            money = input("how much do you want to withdraw?")
+            money = int(money)
+            balences [index] = balences [index] - money
+        elif action == "transfer": 
+            acctname= input("what account do you want to transfer too?")
+            acctname1 = input("what is your second account name?")
+            index = names.index(acctname)
+            index1 = names.index(acctname1)
+            money = input ("how much do you want to transfer from account 1 to 2?")
+            money = int(money)
+            balences [index]= balences [index] + money 
+            balences [index1]= balences [index1] - money 
+        elif action == "remove account": 
+            acctname = input("what account do you wish to remove?")    
+            index = names.index(acctname)
+            names.pop
+            balences.pop
             
+            
+        elif action == "quit": 
+                Check = True 
+                
            
