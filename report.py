fe0b058e5686ce524bc82b4682dfc453061ca66e
coveys880 <coveys27@chcstudent.com>
2025-11-06 10:15:27 -0500

    .py

--- report.py
+++ report.py
@@ -1,8 +1,15 @@
 file = open("day1_20.txt", "r")
 buffer = file.readlines()
 file.close()
-MSFT = []
-AMZN = []
-NVDA = []
-MSFTLine = buffer[0](strip.()) 
-AMZNLine = buffer
+
+MSFT = buffer[0].strip().split(",")
+AMZN = buffer[1].strip().split(",")
+NVDA = buffer[2].strip().split(",")
+
+MSFT.pop(0)
+print(MSFT)
+AMZN.pop(1)
+print(AMZN)
+NVDA.pop(2)
+print(NVDA)
+
